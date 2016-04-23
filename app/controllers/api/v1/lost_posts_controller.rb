6cf4c0609388de85409a8765f9ab550ead4e8ffa
wingleungchoi <wingleungchoi@gmail.com>
2016-04-23 18:45:48 +0800

    fix comma

--- app/controllers/api/v1/lost_posts_controller.rb
+++ app/controllers/api/v1/lost_posts_controller.rb
@@ -13,7 +13,7 @@ module Api
 
     private
       def post_params
-        fishing_gear_attributes = [:category_id. :name ,:mesh_size ,:length ,:width ,:weight ,:color ,:comment ,:production_country_alpha2]
+        fishing_gear_attributes = [:category_id, :name ,:mesh_size ,:length ,:width ,:weight ,:color ,:comment ,:production_country_alpha2]
         params.require(:post).permit(:is_complete, :title, :longitude, :latitude, :radius, :report_date, fishing_gear_attributes: fishing_gear_attributes)
       end
     end
